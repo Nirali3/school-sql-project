a6e567fdde0454c5ee49b590e1166a46efc2a417
Nirali3 <158858965+Nirali3@users.noreply.github.com>
2025-10-27 14:25:08 -0400

    Create columns.sql

---
+++ columns.sql
@@ -0,0 +1,29 @@
+---- students table column names
+1. student_id
+2. first_name
+3. last_name
+4. city
+5. grade_level
+
+----- teachers table column names
+1. teacher_id
+2. first_name
+3. last_name
+4. subject
+
+------ courses table column names
+1. course_id
+2. course_name
+3. teacher_id
+
+
+------ departments table column names
+1. department_id
+2. department_name
+
+
+------ enrollments table column names
+1. enrollment_id
+2. student_id
+3. course_id
+4. grade
