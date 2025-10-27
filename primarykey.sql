125f5a9f1854a9501d14a09f89c34940956b2961
Nirali3 <158858965+Nirali3@users.noreply.github.com>
2025-10-27 14:27:33 -0400

    Create primarykey.sql

---
+++ primarykey.sql
@@ -0,0 +1,14 @@
+---- students table
+student_id PRIMARY KEY
+
+---- teachers table
+teacher_id PRIMARY KEY
+
+---- courses table
+course_id PRIMARY KEY
+
+---- departments table
+department_id PRIMARY KEY
+
+---- enrollments table
+enrollment_id PRIMARY KEY
