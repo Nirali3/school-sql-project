0e60644de2e7fe7dd3e8bd0ed9ba9087e45b4732
Nirali3 <158858965+Nirali3@users.noreply.github.com>
2025-10-27 17:51:26 -0400

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,40 +2,40 @@
 
 --- Students table
 INSERT INTO students VALUES
-(1, 'Tom', 'Smith', 'Newark', 10)
-(2, 'Bob', 'Johnson', 'Jersey City', 11)
+(1, 'Tom', 'Smith', 'Newark', 10),
+(2, 'Bob', 'Johnson', 'Chicago', 11),
 (3, 'Joe', 'Dims', 'New York', 12);
 
---- Teachers table
+-- Teachers
 INSERT INTO teachers VALUES
-(1, 'Nancy', 'Adams', 'Math')
-(2, 'Laura', 'Alexis', 'Science')
+(1, 'Nancy', 'Adams', 'Math'),
+(2, 'Laura', 'Alexis', 'Science'),
 (3, 'David', 'Mark', 'English');
 
---- Courses table
+-- Courses
 INSERT INTO courses VALUES
-(101, 'Algebra I', 1)
-(102, 'Biology', 2)
-(103, 'Literature', 3)
-(104, 'Geometry', 1)
-(201, 'Physics', 2)
+(101, 'Algebra I', 1),
+(102, 'Biology', 2),
+(103, 'Literature', 3),
+(104, 'Geometry', 1),
+(201, 'Physics', 2),
 (202, 'Algebra II', 1);
 
---- departments table
+-- Departments
 INSERT INTO departments VALUES
-(1000, 'Math', 1)
-(1001, 'Science', 2)
+(1000, 'Math', 1),
+(1001, 'Science', 2),
 (1002, 'English', 3);
 
---- enrollments table
+-- Enrollments
 INSERT INTO enrollments VALUES
-(1, 1, 101, 'B')
-(2, 2, 102, 'C')
-(3, 2, 102, 'A')
-(4, 3, 103, 'A')
-(5, 1, 202, 'B')
-(6, 1, 201, 'C')
-(7, 2, 104, 'A')
-(8, 2, 201, 'B')
-(9, 3, 104, 'A')
-(10. 3, 201, 'A');
+(1, 1, 101, 'B'),
+(2, 2, 102, 'C'),
+(3, 2, 103, 'A'),
+(4, 3, 103, 'A'),
+(5, 1, 202, 'B'),
+(6, 1, 201, 'C'),
+(7, 2, 104, 'A'),
+(8, 2, 201, 'B'),
+(9, 3, 104, 'A'),
+(10, 3, 201, 'A');
